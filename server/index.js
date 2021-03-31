e1591294fa6e238ce3a41070e471507609419265
이종범 <n113345@gamil.com>
2021-03-31 16:58:20 +0900

    redis 추가

--- server/index.js
+++ server/index.js
@@ -12,32 +12,62 @@ const io = require("socket.io")(http, {
   },
 });
 
-if (!port) {
-  throw new Error("PORT Undefined");
+const REDIS_HOST = process.env.NODE_ENV === "production" ? "54.180.15.252" : "";
+
+const redis = require("redis");
+
+try {
+  const publisher = redis.createClient({
+    host: REDIS_HOST,
+    port: "6379",
+  });
+  const subscriber = redis.createClient({
+    host: REDIS_HOST,
+    port: "6379",
+  });
+
+  if (!port) {
+    throw new Error("PORT Undefined");
+  }
+
+  app.use(cors());
+
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("/test", (req, res) => {
+    console.log("Get /test");
+    res.send(true);
+  });
+
+  app.get("/check", (req, res) => {
+    res.send(true);
+  });
+
+  app.post("/hifive", (req, res) => {
+    console.log("Post /hifive");
+    publisher.publish("hifive", "hifive");
+    res.send(true);
+  });
+
+  subscriber.on("message", (channel, message) => {
+    if (message === "hifive") {
+      io.emit("hifive");
+    }
+  });
+
+  subscriber.subscribe("hifive");
+
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+
+  io.on("connect", (socket) => {
+    console.log("connected");
+  });
+
+  http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+} catch (error) {
+  console.log(error);
 }
-
-app.use(cors());
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("/check", (req, res) => {
-  res.send(true);
-});
-
-app.post("/hifive", (req, res) => {
-  console.log("Post /hifive");
-  io.emit("hifive");
-  res.send(true);
-});
-
-app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
-io.on("connect", (socket) => {
-  console.log("connected");
-});
-
-http.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
